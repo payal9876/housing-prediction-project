c6d36d30a37cd2cf52e8bf646b9093d3f071d604
payal9876 <payalyadav9456444@gmail.com>
2022-07-16 17:28:46 +0530

    pipeline

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
 
 app=Flask(__name__)
 
@@ -7,6 +9,12 @@ app=Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index():
+    try:
+        raise Exception("we are testing exception")
+    except HousingException as e :
+        logging.info (e)
+
+    logging.info("logging pipeline checking")
     return "this is machine learning project"
 
 
